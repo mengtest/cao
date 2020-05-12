4a1882c684fddf39924dc2b4535fa5d89087b885
moqihome <865567607@qq.com>
2020-05-12 09:38:03 +0800

    是是是

---
+++ 整合包/202005/78如：主线湖畔镇/20200511[终焉誓约][任务脚本]如：主线湖畔镇(倪祖伟)V1.0/刷库sql/20200511[终焉誓约][任务脚本]如：主线湖畔镇(倪祖伟)V1.0.sql
@@ -0,0 +1,26 @@
+###################################################
+#20200511[终焉誓约][任务脚本]如：主线湖畔镇(倪祖伟)
+#by:倪祖伟
+#date:2020-05-11
+#论坛链接:如：http
+###################################################
+#注释部分
+
+#打开注释部分
+
+###################################################
+UPDATE `monster` SET `type`=2 WHERE `id`=1000840;
+UPDATE `monster` SET `type`=2 WHERE `id`=1000850;
+UPDATE `monster` SET `type`=2 WHERE `id`=1000860;
+UPDATE `monster` SET `type`=2 WHERE `id`=1000865;
+UPDATE `monster` SET `type`=2 WHERE `id`=1000870;
+UPDATE `monster` SET `type`=2 WHERE `id`=1000963;
+UPDATE `monster` SET `type`=2 WHERE `id`=1000983;
+UPDATE `monster` SET `type`=2 WHERE `id`=1001705;
+UPDATE `monster` SET `type`=2 WHERE `id`=1001715;
+UPDATE `monster` SET `type`=2 WHERE `id`=1001725;
+UPDATE `monster` SET `type`=2 WHERE `id`=1001730;
+UPDATE `monster` SET `type`=2 WHERE `id`=1001735;
+UPDATE `monster` SET `type`=2 WHERE `id`=1001740;
+UPDATE `monster` SET `type`=2 WHERE `id`=1001745;
+UPDATE `monster` SET `type`=2 WHERE `id`=1001750;
